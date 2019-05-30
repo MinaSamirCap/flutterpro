6843e3743937ea219d92710f081fb1ea5fb1f77a
Mina Samir <MenaSamerCap@gmail.com>
2019-05-30 21:20:04 +0200

    finished the statefulWidget

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,11 +1,13 @@
 import 'package:first_flutter_app/ui/gestures_home_2.dart';
+import 'package:first_flutter_app/ui/make_it_rain_home_2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Gestures",
-    home: GesturesHome2(title: "GES",),
+    //home: GesturesHome2(title: "GES",),
     //home: GesturesHome2("Gestures2", ""),
+    home: MakeItRain(),
   ));
 }
--- lib/ui/make_it_rain_home_2.dart
+++ lib/ui/make_it_rain_home_2.dart
@@ -9,6 +9,21 @@ class MakeItRain extends StatefulWidget {
 }
 
 class MakeItRainState extends State<MakeItRain> {
+  int _moneyCounter = 0;
+
+  MaterialColor _determineColor() {
+    return _moneyCounter> 500? Colors.red : Colors.blue;
+  }
+
+  void _rainMoney() {
+
+    /// this is so important setState must be called
+    /// every time we need to update the UI
+    setState(() {
+      _moneyCounter = _moneyCounter + 100;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,25 +44,24 @@ class MakeItRainState extends State<MakeItRain> {
             )),
             Expanded(
               child: Center(
-                child: Text("000000",
+                child: Text('\$ $_moneyCounter',
                     style: TextStyle(
-                        color: Colors.lightGreen,
+                        color: _determineColor(),
                         fontWeight: FontWeight.w900,
                         fontSize: 50.5)),
               ),
             ),
             Expanded(
-              child: Center(
-                child: FlatButton(
-                    onPressed: ()=> debugPrint("HEY"),
-                    color: Colors.lightBlue,
-                    child: Text("Let it rain",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 20.5))),
-              )
-            )
+                child: Center(
+              child: FlatButton(
+                  onPressed: _rainMoney,
+                  color: Colors.lightBlue,
+                  child: Text("Let it rain",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20.5))),
+            ))
           ],
         ),
       ),
