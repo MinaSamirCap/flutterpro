047cff56c1fffaa56d9455203761b606cc28c5a6
unknown <menasamercap@gmail.com>
2019-06-01 13:08:07 +0200

    added the two button with the validations

--- lib/ui/CustomLoginForm.dart
+++ lib/ui/CustomLoginForm.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
-
-
+import 'package:flutter/material.dart';
 
 class CustomLoginForm extends StatefulWidget {
   @override
@@ -8,9 +7,96 @@ class CustomLoginForm extends StatefulWidget {
 }
 
 class _CustomLoginFormState extends State<CustomLoginForm> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hello World");
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // add image
+          Center(
+            child: Image.asset("images/google.png",
+                width: 90, height: 90, color: Colors.white),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  icon: Icon(Icons.verified_user),
+                  labelText: "Name",
+                  border: OutlineInputBorder(
+                      gapPadding: 4, borderRadius: BorderRadius.circular(3.3))),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "please enter name";
+                } else {
+                  debugPrint("All is Good $value");
+                }
+              },
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                      gapPadding: 4, borderRadius: BorderRadius.circular(3.3))),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "please enter password";
+                } else {
+                  debugPrint("all is good $value");
+                }
+              },
+            ),
+          ),
+
+          //// adding buttons ... :)
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Scaffold.of(context)
+                            .showSnackBar(SnackBar(content: Text("GOOOOD!!!")));
+                      } else {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text("BAAAAAAAAAD!!!")));
+                      }
+                    },
+                    child: Text("Submit"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      _formKey.currentState.reset();
+                    },
+                    child: Text("Clear"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Center(child: Text("Hello There")),
+          )
+        ],
+      ),
+    );
   }
 }
