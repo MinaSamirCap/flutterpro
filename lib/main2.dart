5b6ee878cdcfc2860febd04a8bee65de0189603c
unknown <menasamercap@gmail.com>
2019-06-01 11:31:47 +0200

    added the initila code for the login app

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,13 +1,35 @@
+import 'package:first_flutter_app/ui/CustomLoginForm.dart';
 import 'package:first_flutter_app/ui/gestures_home_2.dart';
 import 'package:first_flutter_app/ui/make_it_rain_home_2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+/*void main() {
   runApp(MaterialApp(
     title: "Gestures",
     //home: GesturesHome2(title: "GES",),
     //home: GesturesHome2("Gestures2", ""),
     home: MakeItRain(),
   ));
+}*/
+
+void main() => runApp(Login2());
+
+class Login2 extends StatelessWidget {
+  final appTitle = "login";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: CustomLoginForm(),
+      ),
+    );
+  }
 }
---
+++ lib/ui/CustomLoginForm.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+
+
+class CustomLoginForm extends StatefulWidget {
+  @override
+  _CustomLoginFormState createState() => _CustomLoginFormState();
+}
+
+class _CustomLoginFormState extends State<CustomLoginForm> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello World");
+  }
+}
