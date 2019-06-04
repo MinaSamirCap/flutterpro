7f318ab8db3cd6b02d886d934414385c74015791
unknown <menasamercap@gmail.com>
2019-06-04 17:27:07 +0200

    added style in a separate funtion

--- lib/ui/klimatic_app.dart
+++ lib/ui/klimatic_app.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,11 +12,46 @@ class _KlimaticState extends State<Klimatic> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text("Klimatic", style: TextStyle(color: Colors.black),),
+        title: Text(
+          "Klimatic",
+          style: TextStyle(color: Colors.black),
+        ),
         actions: <Widget>[
-
+          IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.red,
+            ),
+            onPressed: () => print("pressed"),
+          )
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          SizedBox.expand(
+            child: Image.asset(
+              "images/umbrella.png",
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            alignment: Alignment.topRight,
+            margin: const EdgeInsetsDirectional.fromSTEB(0, 10, 20, 0),
+            child: Text(
+              "Spokane!!",
+              style: cityStyle(),
+            ),
+          )
         ],
       ),
     );
+  }
+
+  cityStyle() {
+    return TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w900,
+        fontStyle: FontStyle.italic,
+        fontSize: 25);
   }
 }
